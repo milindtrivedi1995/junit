0315b0ee894dea5096d5f877b96d4ca2d5c32244
100891 <100891@ALDCPMOBAP01.aegonlife.com>
2019-03-27 12:36:05 +0530

    First

---
+++ JUnitTest/src/com/TestJunit.java
@@ -0,0 +1,25 @@
+package com;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TestJunit {
+	
+   String message = "Hello World";	
+   MessageUtil messageUtil = new MessageUtil(message);
+
+   @Test
+   public void testPrintMessage() {	  
+      assertEquals(message,messageUtil.printMessage());
+      
+   }
+   @Test
+   public void testMessage() {	  
+      assertEquals("Hello"+" World",messageUtil.printMessage());
+   }
+   
+   @Test
+   public void testMilindMessage() {	  
+      assertEquals("Milind",messageUtil.printMessage());
+   }
+}
